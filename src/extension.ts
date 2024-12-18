cb50736bc10f9de0aed6d71374b69d684b0e8f27
Radoslaw Szwajkowski <rszwajko@redhat.com>
2024-12-18 19:07:50 +0100

    Extract applyBlock command to sample extension
    Reference-Url: https://github.com/microsoft/vscode/issues/234097
    Reference-Url: https://github.com/konveyor/editor-extensions/blob/6bdca4cc8318197f86d0d0a1e4b4da8a5563d7b6/vscode/src/diffView/solutionCommands.ts#L77
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "diff-regular-files" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('diff-regular-files.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from diff-regular-files!');
-	});
-
-	context.subscriptions.push(disposable);
+interface ApplyBlockArgs {
+  mapping: { original: vscode.Range; innerChanges: unknown };
+  modifiedUri: vscode.Uri;
+  originalUri: vscode.Uri;
+  originalWithModifiedChanges: string;
 }
 
-// This method is called when your extension is deactivated
+export const applyBlock = async ({
+  originalUri,
+  originalWithModifiedChanges,
+}: ApplyBlockArgs) => {
+  const doc = vscode.workspace.textDocuments.find(
+    (doc) => doc.uri.toString() === originalUri.toString()
+  );
+
+  if (!doc) {
+    vscode.window.showErrorMessage(
+      `Failed to retrieve editor for ${originalUri}`
+    );
+    console.error(`Failed to retrieve editor for ${originalUri}`);
+    return;
+  }
+
+  const edit = new vscode.WorkspaceEdit();
+  edit.replace(
+    originalUri,
+    new vscode.Range(0, 0, doc.lineCount, 0),
+    originalWithModifiedChanges
+  );
+
+  // note that left side edits will open a duplicated editor
+  // bug opened: https://github.com/microsoft/vscode/issues/234097
+  return vscode.workspace.applyEdit(edit);
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("diff-regular-files.applyBlock", applyBlock)
+  );
+}
+
 export function deactivate() {}
